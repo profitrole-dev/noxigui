c45e777e04c97048f3e20ddd8145c7b14b209cbc
profitrole-dev <dev.github@asharshov.com>
2025-08-28 23:26:31 +0200

    feat: add package build script

---
+++ scripts/build-packages.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const path = require('path');
+const { execSync } = require('child_process');
+
+const packagesDir = path.join(__dirname, '..', 'packages');
+const packageDirs = fs
+  .readdirSync(packagesDir)
+  .filter((name) => fs.statSync(path.join(packagesDir, name)).isDirectory());
+
+const packages = packageDirs
+  .map((dir) => {
+    const pkgJsonPath = path.join(packagesDir, dir, 'package.json');
+    const pkgJson = JSON.parse(fs.readFileSync(pkgJsonPath, 'utf8'));
+    return {
+      name: pkgJson.name,
+      deps: Object.assign({}, pkgJson.dependencies, pkgJson.devDependencies),
+    };
+  })
+  .filter((pkg) => pkg.name !== '@noxigui/playground');
+
+// Build dependency graph and perform topological sort
+const graph = new Map();
+for (const pkg of packages) {
+  const deps = Object.keys(pkg.deps || {}).filter((dep) => dep.startsWith('@noxigui/'));
+  graph.set(pkg.name, deps);
+}
+
+const visited = new Set();
+const order = [];
+function visit(name, stack = new Set()) {
+  if (visited.has(name)) return;
+  if (stack.has(name)) throw new Error('Circular dependency detected');
+  stack.add(name);
+  const deps = graph.get(name) || [];
+  for (const dep of deps) {
+    if (graph.has(dep)) visit(dep, stack);
+  }
+  stack.delete(name);
+  visited.add(name);
+  order.push(name);
+}
+
+for (const name of graph.keys()) {
+  visit(name);
+}
+
+for (const name of order) {
+  console.log(`Building ${name}...`);
+  execSync(`pnpm -F ${name} build`, { stdio: 'inherit' });
+}
