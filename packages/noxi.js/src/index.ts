67e90bca4de0001cc4042b949b0fba1485506647
profitrole-dev <dev.github@asharshov.com>
2025-08-29 14:47:35 +0200

    fix: export modules explicitly

--- packages/noxi.js/src/index.ts
+++ packages/noxi.js/src/index.ts
@@ -2,6 +2,8 @@ import { RuntimeInstance, type GuiObject, type Renderer } from '@noxigui/runtime
 import { createPixiRenderer } from '@noxigui/renderer-pixi';
 import { Parser } from '@noxigui/parser';
 
+export { RuntimeInstance, createPixiRenderer, Parser };
+
 const Noxi = {
   gui: {
     create(xml: string, renderer: Renderer = createPixiRenderer()): GuiObject {
