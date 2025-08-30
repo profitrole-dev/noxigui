0639d1a75618b6860141b8bb4460ebc4814669fd
profitrole-dev <dev.github@asharshov.com>
2025-08-30 11:32:03 +0200

    feat: add WrapPanel layout

--- packages/parser/src/parsers/index.ts
+++ packages/parser/src/parsers/index.ts
@@ -3,6 +3,7 @@ export { TextBlockParser } from './TextBlockParser.js';
 export { BorderParser } from './BorderParser.js';
 export { StackPanelParser } from './StackPanelParser.js';
 export { DockPanelParser } from './DockPanelParser.js';
+export { WrapPanelParser } from './WrapPanelParser.js';
 export { GridParser } from './GridParser.js';
 export { ImageParser } from './ImageParser.js';
 export { ResourcesParser } from './ResourcesParser.js';
@@ -14,6 +15,7 @@ import { TextBlockParser } from './TextBlockParser.js';
 import { BorderParser } from './BorderParser.js';
 import { StackPanelParser } from './StackPanelParser.js';
 import { DockPanelParser } from './DockPanelParser.js';
+import { WrapPanelParser } from './WrapPanelParser.js';
 import { GridParser } from './GridParser.js';
 import { ImageParser } from './ImageParser.js';
 import { ResourcesParser } from './ResourcesParser.js';
@@ -28,6 +30,7 @@ export const createParsers = (templates: TemplateStore): ElementParser[] => [
   new BorderParser(),
   new StackPanelParser(),
   new DockPanelParser(),
+  new WrapPanelParser(),
   new GridParser(),
   new ImageParser(),
   new ResourcesParser(templates),
