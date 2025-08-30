278b2855cd34568ea1d6b126cc659f491e421651
profitrole-dev <dev.github@asharshov.com>
2025-08-30 15:17:22 +0200

    feat(studio): add JSON editor for Data tab

--- packages/studio/src/tabs/DataTab.tsx
+++ packages/studio/src/tabs/DataTab.tsx
@@ -1,6 +1,21 @@
-import React from "react";
+import MonacoEditor from "react-monaco-editor";
+import { useStudio } from "../state/useStudio";
 
 export function DataTab() {
-  return <div className="p-2">Data</div>;
+  const { project, setData } = useStudio();
+  return (
+    <MonacoEditor
+      language="json"
+      value={JSON.stringify(project.data, null, 2)}
+      onChange={(v) => {
+        try {
+          setData(JSON.parse(v));
+        } catch {}
+      }}
+      options={{ minimap: { enabled: false }, fontSize: 14 }}
+      width="100%"
+      height="100%"
+    />
+  );
 }
 
